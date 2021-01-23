6c19a551ea969ca50fa49070495e5a2658f4a45c
Jasper Eenhoorn <eenhoorn.jasper@gmail.com>
2021-01-23 06:36:59 +0100

    23.  the field object also keeps its own scores # TODO: move 24.  colored square behind white square, try to move labels away 25.  separately draw green margin 26.  function to write green labels 27.  proto square function to separate some logic 28.  function to write oranges labels 29.  function to write purples less than signs 30.  move these color specific checks in play and cross out of field class 31.  in fact, contain color-value-rect-dots in one object 32.  draws rect and dots to layer 33.  in fact, change to list of Die instances 34.  draw silver plate and its dice in the newer fashion 35.  write the bound union function in the newer fashion 36.  rewrite the die class to fit the following usage 37.  arrange the dice in a smart way 38.  getters and setters for color and value 39.  proto function for adding square 40.  layer index not working #FIXME 41.  throw the die and refresh the sprites

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import wasabi2d as w
 import random # needed for die
+from functools import reduce # for bounds union
 
 # (3) define some _pretty_ distinguishable colors
 colors = ["#fed402","#0477bb","#02a039",
@@ -18,67 +19,35 @@ class Fields(w.Group):
     def __init__(self, layer, ypos=25, color=None):
         super().__init__(self)
         self.layer = layer
-        self.scores = []
-# (17) need to keep track of req labels
-        self.reqs = []
         self.color = color
         self.whites = [self.square(i, ypos) for i in range(11)]
-# (7) labels for green fields in top margin
+# (23) the field object also keeps its own scores # TODO: move
+        self.scores = []
+# (27) proto square function to separate some logic
+    def _square(self, pos, color='white', size=30):
+        return self.layer.add_rect(
+                color=color,width=size,height=size,pos=pos)
+# (24) colored square behind white square, try to move labels away
     def square(self, i, ypos):
-        size = 40
-        pos = (25+35*i,ypos)
-        height = 60 if self.color == colors[2] else size
-        self.layer.add_rect(
-            color=self.color,width=size,height=height,pos=pos)
-        size = height = 30
-        white = self.layer.add_rect(
-            color='white',width=size,height=height,pos=pos)
-        if self.color == colors[2]:
-            self.layer.add_label(str(int(0.5*(i+1)*(i+2))),
-                    color='black',fontsize=11,align='center',
-                    pos=(pos[0]+2,pos[1]-18))
-# (15) labels for green field requirements
-            self.reqs.append(self.layer.add_label("≥"+"12345123456"[i],
-                    color='black',fontsize=11,align='center',
-                    pos=(pos[0]+2,pos[1]+4)))
-# (8) labels for oranges fields in squares
-        if self.color == colors[3]:
-            if i in [3, 6, 8, 10]:
-                self.layer.add_label("x2",
-                        color=colors[3],fontsize=13,align='center',
-                        pos=(pos[0]+2,pos[1]+4))
-# (9) less than sign polygons for purple fields
-        if self.color == colors[4]:
-            if i > 0:
-                self.lt(pos)
+        pos   = (25+35*i,ypos)
+        bg    = self._square(pos, color=self.color, size=40)
+        white = self._square(pos)
         return white
-    def lt(self, pos):
-        _lt_bg = self.layer.add_polygon(color=colors[4], fill=True, vertices=\
-                        [(-5,0),
-                         (+8,-9),
-                         (+8,+9),
-                        ], pos=(pos[0]-20,pos[1]))
-        _lt_fg = self.layer.add_polygon(color='white', fill=True, vertices=\
-                        [(-1,0),
-                         (+6,-5),
-                         (+6,+5),
-                        ], pos=(pos[0]-20,pos[1]))
-        return (_lt_bg, _lt_fg)
-
 # (13) return false if invalid move
     def play(self, value):
+# (30) move these color specific checks in play and cross out of field class
         if self.color == colors[2]: # green
             if len(self.scores) < 11:
                 if int("12345123456"[len(self.scores)]) <= value:
                     self.scores.append(True)
                     self.cross(value)
-                    print(f"Scored {len(self.scores)}")
+                    # print(f"Scored {len(self.scores)}")
                     return True
         if self.color == colors[3]: # orange
             if len(self.scores) < 11:
                 self.scores.append(True)
                 self.cross(value)
-                print(f"Scored {len(self.scores)}")
+                # print(f"Scored {len(self.scores)}")
                 return True
         if self.color == colors[4]: # purple
             if len(self.scores) < 11:
@@ -86,7 +55,7 @@ class Fields(w.Group):
                         or self.scores[-1] == 6:
                     self.scores.append(value)
                     self.cross(value)
-                    print(f"Scored {len(self.scores)}")
+                    # print(f"Scored {len(self.scores)}")
                     return True
         return False
     def cross(self, value):
@@ -98,71 +67,195 @@ class Fields(w.Group):
             self.layer.add_label("X",pos=add((3,2),tpos),
                     align='center',fontsize=17, color='black')
             self.reqs[cindex].delete()
-            print(len(self.reqs))
+            # print(len(self.reqs))
         if self.color in [colors[3], colors[4]]: # orange or purple
             self.layer.add_label(str(value),align='center',fontsize=13,
                     color='black',pos=tpos)
+
+# (25) separately draw green margin
+scene.layers[10].add_rect(color=colors[2],width=scene.width-10,height=15,
+        pos=(scene.width/2,scene.height-130))
 
 tops    = [scene.height-d for d in [105,65,25]] # actually ypos
 greens  = Fields(scene.layers[10], ypos=tops[0], color=colors[2])
 oranges = Fields(scene.layers[10], ypos=tops[1], color=colors[3])
 purples = Fields(scene.layers[10], ypos=tops[2], color=colors[4])
 
+# (26) function to write green labels
+def green_labels(greens):
+# (7) labels for green fields in top margin
+    for i, green in enumerate(greens.whites):
+        greens.layer.add_label(str(int(0.5*(i+1)*(i+2))),
+                color='black',fontsize=11,align='center',
+                pos=(green.pos[0]+2,green.pos[1]-19))
+# (15) labels for green field requirements, saved in greens group
+# (17) in order to delete the label as the greens are filled
+        if 'reqs' not in greens.__dict__: greens.reqs = []
+        greens.reqs.append(greens.layer.add_label("≥"+"12345123456"[i],
+                color='black',fontsize=11,align='center',
+                pos=(green.pos[0]+2,green.pos[1]+4)))
+green_labels(greens)
+
+# (28) function to write oranges labels
+def orange_labels(oranges):
+# (8) labels for oranges fields in squares
+    for i, orange in enumerate(oranges.whites):
+        if i in [3, 6, 8, 10]:
+            oranges.layer.add_label("x2",
+                    color=colors[3],fontsize=13,align='center',
+                    pos=(orange.pos[0]+2,orange.pos[1]+4))
+orange_labels(oranges)
+
+# (29) function to write purples less than signs
+def purple_labels(purples):
+    def lt(self, pos):
+        _lt_bg = self.layer.add_polygon(color=colors[4], fill=True, vertices=\
+                        [(-5,0), (+8,-9), (+8,+9)], pos=(pos[0]-20,pos[1]))
+        _lt_fg = self.layer.add_polygon(color='white', fill=True, vertices=\
+                        [(-1,0), (+6,-5), (+6,+5)], pos=(pos[0]-20,pos[1]))
+        return (_lt_bg, _lt_fg)
+# (9) less than sign polygons for purple fields
+    for i, purple in enumerate(purples.whites):
+        if i > 0:
+            lt(purples, purple.pos)
+purple_labels(purples)
+
+# (22) include the color index in the tuple
+# (31) in fact, contain color-value-rect-dots in one object
+class Die():
+    def __init__(self, layer, color=0, value=None): # layer required
+        self.color = color
+        self.value = value if value else self._throw()
+        self.layer = layer
+# (32) draws rect and dots to layer
+        self.rect  = self._rect()
+        self.dots  = w.Group(self._dots())
+        self.bounds = self.rect.bounds
+# (38) getters and setters for color and value
+    @property
+    def color(self):
+        return self._color
+    @color.setter
+    def color(self, color):
+        self._color = color
+    @property
+    def value(self):
+        return self._value
+    @value.setter
+    def value(self, value):
+        self._value = value
+# (39) proto function for adding square
+    def _rect(self):
+        return self.layer.add_rect(color=colors[self.color],width=30,height=30)
+# (5) die needs to have dots drawn
+    def _dots(self):
+        pos = self.rect.pos
+        if self.value == 1:
+            return [self._dot(pos)]
+        if self.value == 2:
+            return [self._dot(pos,(-6,6)),
+                    self._dot(pos,(6,-6)),]
+        if self.value == 3:
+            return [self._dot(pos,(-8,8)),
+                    self._dot(pos,),
+                    self._dot(pos,(8,-8)),]
+        if self.value == 4:
+            return [self._dot(pos,(6,-6)),
+                    self._dot(pos,(6,6)),
+                    self._dot(pos,(-6,6)),
+                    self._dot(pos,(-6,-6)),]
+        if self.value == 5:
+            return [self._dot(pos,(-8,8)),
+                    self._dot(pos,(8,8)),
+                    self._dot(pos,),
+                    self._dot(pos,(-8,-8)),
+                    self._dot(pos,(8,-8)),]
+        if self.value == 6:
+            return [self._dot(pos,(8,-6)),
+                    self._dot(pos,(8,6)),
+                    self._dot(pos,(0,6)),
+                    self._dot(pos,(0,-6)),
+                    self._dot(pos,(-8,6)),
+                    self._dot(pos,(-8,-6)),]
+        return []
+    def _dot(self, pos, rel=(0,0)):
+        pos = add(pos,rel)
+        return self.layer.add_circle(radius=2.7,color='#111111',pos=pos)
+    def move(self, pos):
+        self.rect.pos = pos
+        self.dots.pos = pos
+# (41) throw the die and refresh the sprites
+    def _throw(self):
+        return random.randint(1,6)
+    def throw(self):
+        self.value = self._throw()
+        self.dots.delete()
+        self.dots = w.Group(self._dots())
+        return self.dots
+
 # (2) die needs to be thrown, drawn and grouped (no dots)
 class Dice(w.Group):
-# (20) allow the making of empty dice object
-    def __init__(self, layer, empty=False):
+    def __init__(self, layer, dice=[], ypos=25):
         super().__init__(self)
         self.layer = layer
-        self.pos = (0, 0)
-        # self.dice = [self.die(i) for i in range(6)]
+        self.ypos  = ypos
 # (18) change from dice to groups of values, rects and dots
-        if not empty:
-            ids, values, rects, dots = zip(*[self.die(i) for i in range(6)])
-            self.ids = list(ids)
-            self.values = list(values)
-            self.rects = w.Group(rects)
-            self.dots = w.Group(dots)
+# (33) in fact, change to list of Die instances
+        if dice == []:
+            self.dice = [Die(layer=self.layer, color=i) for i in range(6)]
+# (20) allow the making of empty dice object
         else:
-            self.ids = []
-            self.values = []
-            self.rects = w.Group([])
-            self.dots = w.Group([])
+            self.dice = dice
+        self.rects = w.Group([die.rect for die in self.dice])
+        self.dots  = w.Group([die.dots for die in self.dice])
+        self.arrange()
 # (19) get the bound union of all the dice bounds
-    def get_bound(self):
-        _bound = self.rects[0].bounds
-        for rect in self.rects[1:]:
-            _bound = _bound.union(rect.bounds)
-        return _bound
-    def die(self, i, value=None):
-        rect = self.layer.add_rect(
-            color=colors[i],width=30,height=30,pos=(35*i,0))
-        value = random.randint(1,6) if not value else value
-        dots = w.Group(self.adddots(rect.pos, value))
-# (22) include the color index in the tuple
-        return (i, value, rect, dots)
+# (35) write the bound union function in the newer fashion
+    def get_bound(self): 
+        bounds = [die.bounds for die in self.dice]
+        return reduce(lambda a, b: a.union(b), bounds)
+        # _bound = bounds[0]
+        # for bound in bounds[1:]:
+        #     _bound = _bound.union(bound)
+        # return _bound
+# (37) arrange the dice in a smart way
+    def arrange(self):
+        n = len(self.dice)
+        [die.move((20+(i-n/2)*35+self.pos[0], self.pos[1]))
+            for i, die in enumerate(self.dice)]
+        self.move((scene.width/2, self.ypos))
     def move(self, pos):
-        pos = add((-self.get_bound().width/2+15,0),pos)
+# (36) rewrite the die class to fit the following usage
         self.rects.pos = pos
-        self.dots.pos  = pos
+        self.dots.pos = pos
+        # pos = add((-self.get_bound().width/2+15,0),pos)
+        # self.rects.pos = pos
+        # self.dots.pos  = pos
     def select(self, pos): # returns index only
-        # return list(filter(lambda x: x[1].bounds.collidepoint(pos), self.dice))[0]
-        return [i for i, rect in enumerate(self.rects) if\
-                rect.bounds.collidepoint(pos)][0]
-        # return [i for i, die in enumerate(self.dice) if # TODO
-        #         die[1].bounds.collidepoint(pos)][0]
+        return [i for i, die in enumerate(self.dice) if\
+                die.rect.bounds.collidepoint(pos)][0]
 # (21) use add and take for moving to dice or to silver
-    def add(self, die): # color index i/c and value v
-        i, v, r, d = die
-        r.delete()
-        d.clear()
-        i, value, rect, dot = self.die(i, v)
-        self.values.append(value)
-        self.rects.append(rect)
-        self.dots.append(dot)
+    def add(self, die): 
+        die.rect.delete()
+        die.dots.clear()
+        print(self.layer)
+        _die = Die(layer=self.layer, color=die.color, value=die.value)
+        print(len(self.rects))
+        self.rects.append(_die.rect)
+        self.dots.append(_die.dots)
+        self.dice.append(_die)
+        print(len(self.rects))
+        self.arrange()
     def take(self, i):
-        return self.ids.pop(i), self.values.pop(i), self.rects.pop(i), self.dots.pop(i)
-    def discard(self, dindex):
+        _list1 = self.rects.explode()
+        _list2 = self.dots.explode()
+        _subl1 = _list1[0:i]+_list1[i+1:]
+        _subl2 = _list2[0:i]+_list2[i+1:]
+        self.rects = w.Group(_subl1)
+        self.dots  = w.Group(_subl2)
+        self.arrange()
+        return self.dice.pop(i)
+    def discard(self, dindex): # TODO
         self.rects[dindex].clear()
         self.dots[dindex].clear()
         self.ids.pop(dindex)
@@ -170,58 +263,26 @@ class Dice(w.Group):
         self.rects.pop(dindex)
         self.dots.pop(dindex)
     def throw(self, i=None):
-        # values, rects, dots = zip(*self.dice)
-        [dot.clear() for dot in self.dots]
-        self.ids    = [0, 1, 2, 3, 4, 5]
-        self.values = [random.randint(1,6) for i in range(6)]
-        self.dots   = [w.Group(self.adddots(rect.pos, value)) 
-                for rect, value in zip(self.rects, self.values)]
-        # self.dice = list(zip(new_values, rects, new_dots))
-# (5) die needs to have dots drawn
-    def adddots(self, pos, value):
-        if value == 1:
-            return [self.dot(pos)]
-        if value == 2:
-            return [self.dot(pos,(-6,6)),
-                    self.dot(pos,(6,-6)),]
-        if value == 3:
-            return [self.dot(pos,(-8,8)),
-                    self.dot(pos,),
-                    self.dot(pos,(8,-8)),]
-        if value == 4:
-            return [self.dot(pos,(6,-6)),
-                    self.dot(pos,(6,6)),
-                    self.dot(pos,(-6,6)),
-                    self.dot(pos,(-6,-6)),]
-        if value == 5:
-            return [self.dot(pos,(-8,8)),
-                    self.dot(pos,(8,8)),
-                    self.dot(pos,),
-                    self.dot(pos,(-8,-8)),
-                    self.dot(pos,(8,-8)),]
-        if value == 6:
-            return [self.dot(pos,(8,-6)),
-                    self.dot(pos,(8,6)),
-                    self.dot(pos,(0,6)),
-                    self.dot(pos,(0,-6)),
-                    self.dot(pos,(-8,6)),
-                    self.dot(pos,(-8,-6)),]
-        return []
-    def dot(self, pos, rel=(0,0)):
-        pos = add(pos,rel)
-        return self.layer.add_circle(
-                radius=2.7,color='#111111',pos=pos)
+        for die in self.dice:
+            dots = die.throw() 
+            self.dots.append(dots)
+        # self.arrange()
+        # # values, rects, dots = zip(*self.dice)
+        # [dot.clear() for dot in self.dots]
+        # self.ids    = [0, 1, 2, 3, 4, 5]
+        # self.values = [random.randint(1,6) for i in range(6)]
+        # self.dots   = [w.Group(self.adddots(rect.pos, value)) 
+        #         for rect, value in zip(self.rects, self.values)]
+        # # self.dice = list(zip(new_values, rects, new_dots))
     def __str__(self):
-        return ", ".join([f"{i}:{v}" for i, v in zip(self.ids, self.values)])
+        return ", ".join([f"{die.color}:{die.value}" for die in self.dice])
 
 dice = Dice(scene.layers[20])
-dice.move((scene.width/2, 25))
 
-scene.layers[30].add_rect(color='#444444',width=scene.width,
+# (34) draw silver plate and its dice in the newer fashion
+scene.layers[10].add_rect(color='#444444',width=scene.width,
         height=40,pos=(scene.width/2, 70))
-silver = Dice(scene.layers[30], empty=True)
-silver.centerx = scene.width/2
-# silver.move((scene.width/2, 70))
+silver = None
 
 # (4) press space to test new turn/throwing dice again
 @w.event
@@ -239,6 +300,7 @@ def click_die_then_field(pos):
     if cdtf_expect == 0:
         if my < 60 and mx < 240:
             cdtf_die = dice.select(pos) # index only
+            # print(f'selected {cdtf_die}')
             cdtf_expect = 1
     elif cdtf_expect == 1:
         if my > tops[0]-30: # actually pressing field
@@ -249,10 +311,10 @@ def click_die_then_field(pos):
 # (12) put the value into the field if purple or orange
 # (14) put an X in the field if green
 def play_to_field(pos):
-    global cdtf_expect, cdtf_die
+    global cdtf_expect, cdtf_die, silver
     mx, my  = pos
     success = False
-    value   = dice.values[cdtf_die]
+    value   = dice.dice[cdtf_die].value
 
     if my > tops[0]-10 and my < tops[0]+30:
         success = greens.play(value)
@@ -262,8 +324,13 @@ def play_to_field(pos):
         success = purples.play(value)
     if success:
         cdtf_expect = 0
-        # dice.discard(cdtf_die) # for now, discard die, later silver platter
-        silver.add(dice.take(cdtf_die))
+        if silver is None:
+# (40) layer index not working #FIXME 
+            silver = Dice(scene.layers[30], dice=[dice.take(cdtf_die)],
+                    ypos=70)
+        else:
+            silver.add(dice.take(cdtf_die)) 
+        dice.arrange()
         cdtf_die = None
     else: # do nothing if move was not valid
         cdtf_expect = 0
